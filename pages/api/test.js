2a53f3a2d29a35f12303f3bd7cfb7058a1d7caed
Kyle Smith <sKorpion19091@gmail.com>
2021-11-01 15:01:41 +0200

    :sparkles: Initial commit

---
+++ pages/api/test.js
@@ -0,0 +1,3 @@
+export default function handler(req, res) {
+  res.status(200).json({ data: '🔥 Base API Route is Working' });
+}
---
+++ pages/example.js
@@ -0,0 +1,10 @@
+import Layout from "../Components/layout"
+import utilStyles from "../styles/utils.module.css"
+
+export default function Test() {
+    return (
+        <Layout>
+            <h1 className={utilStyles.headingLg}>Example Page</h1>
+        </Layout>
+    )
+};
